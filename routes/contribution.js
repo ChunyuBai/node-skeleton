b1507d1f0e32bcfb1b08d6615de9d96d5fd2d385
Steve Willsmore <scwillsmore@gmail.com>
2023-01-02 20:22:16 -0500

    Add database content to page for specific story id.

--- routes/contribution.js
+++ routes/contribution.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router  = express.Router();
-const db = require('../db/connection');
 
 router.post('/story_contribution', (req, res) => {
   console.log('story contribution:', req.body.contribution);
--- routes/stories.js
+++ routes/stories.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const router  = express.Router();
-const db = require('../db/connection');
 const database = require('../db/queries/stories');
 
 router.get('/stories/:id', (req, res) => {
   const id = req.params.id;
   const dbEntry = database.getStoryWithId(id);
 
-  const templateVars = {
-    greeting: "Story Name Placeholder",
-    id: id,
-    content: dbEntry
-  };
-  console.log(templateVars);
-  res.render('stories', templateVars);
+  // On resolved promise
+  dbEntry.then(data => {
+    const templateVars = {
+      title: data.name,
+      id: data.id,
+      content: data.content
+    };
+
+    console.log('tVars:', templateVars);
+    res.render('stories', templateVars);
+  });
+
 });
 
 module.exports = router;

d0520ef18667079bf98e249454027256a98e84af
Steve Willsmore <scwillsmore@gmail.com>
2023-01-03 11:23:10 -0500

    Add database content to page for contributions.

--- db/seeds/03_contributions.sql
+++ db/seeds/03_contributions.sql
@@ -4,5 +4,7 @@ INSERT INTO contributions (story_id,author_id,content,upvotes) VALUES
 (2, 2, 'add content 2', 0
 ),
 (3, 2, 'add content 3', 1
+),
+(1, 2, 'add content 4', 4
 )
 ;
--- routes/stories.js
+++ routes/stories.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const router  = express.Router();
-const database = require('../db/queries/stories');
+const storyQuery = require('../db/queries/stories');
+const contributionsQuery = require('../db/queries/contributions');
 
 router.get('/stories/:id', (req, res) => {
   const id = req.params.id;
-  const dbEntry = database.getStoryWithId(id);
+  const story = storyQuery.getStoryWithId(id);
+  const contributions = contributionsQuery.getContributionsWithStoryId(id);
 
   // On resolved promise
-  dbEntry.then(data => {
-    const templateVars = {
-      title: data.name,
-      id: data.id,
-      content: data.content
-    };
+  story.then(storyData => {
+    contributions.then(contributionData => {
 
-    console.log('tVars:', templateVars);
-    res.render('stories', templateVars);
+      const templateVars = {
+        title: storyData.name,
+        id: storyData.id,
+        content: storyData.content,
+        contributions: contributionData
+      };
+
+      console.log('tVars:', templateVars);
+      res.render('stories', templateVars);
+    });
   });
-
 });
 
 module.exports = router;

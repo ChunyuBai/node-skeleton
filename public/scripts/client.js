a7f1b292868252c6cd5307130b7f129a79d374f4
wundeeh <jakiriksh@gmail.com>
2023-01-04 22:28:23 +0000

    links the home page with the stories db and renders them all accordingly

---
+++ public/scripts/client.js
@@ -0,0 +1,47 @@
+function createStoryElement(story) {
+
+  const escape = function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const markup = `
+  <div class="story">
+        <a href="/stories/${story.id}">
+          <div class="storyContent">
+            <span
+              >${escape(story.content)}</span
+            >
+          </div>
+          <hr class="breakLine" />
+          <div class="storyFooter">
+            <div class="status">
+              <p class="${story.is_complete ? "completed": "incomplete"}">${story.is_complete ? "Completed": "In progress"}</p>
+            </div>
+          </div>
+        </a>
+      </div>
+  `;
+
+  return markup;
+}
+
+// Goes through an array of tweets and uses createTweetElement to put them into HTML
+function renderStories(stories) {
+  $("#stories-container").empty();
+  for (let i = 0; i < stories.length; i++) {
+    const newStory = createStoryElement(stories[i]);
+    $('#stories-container').append(newStory);
+
+  }
+}
+
+// Renders the tweets on the page
+function loadStories() {
+  $.get( "/stories", function( data ) {
+    renderStories(data);
+  });
+}
+
+loadStories();
